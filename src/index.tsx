c64622b6f559492eba829de52eb865f49ed575b9
Roman Pushkarev <pushkarevromana@gmail.com>
2025-12-25 19:14:13 +0300

    Revert "1.0"
    This reverts commit ed702a450f34c11d600746473f9ba3d9174208e6.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
-import {
-	ArticleParamsForm,
-	ArticleParamsFormState,
-} from './components/article-params-form/ArticleParamsForm';
+import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
@@ -16,27 +13,19 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [pageStyle, setPageStyle] = useState({
-		'--font-family': defaultArticleState.fontFamilyOption.value,
-		'--font-size': defaultArticleState.fontSizeOption.value,
-		'--font-color': defaultArticleState.fontColor.value,
-		'--container-width': defaultArticleState.contentWidth.value,
-		'--bg-color': defaultArticleState.backgroundColor.value,
-	});
-
 	return (
-		<main className={clsx(styles.main)} style={pageStyle as CSSProperties}>
-			<ArticleParamsForm
-				onApply={(state: ArticleParamsFormState) => {
-					setPageStyle({
-						'--font-family': state.fontFamily.value,
-						'--font-size': state.fontSize.value,
-						'--font-color': state.fontColor.value,
-						'--container-width': state.contentWidth.value,
-						'--bg-color': state.backgroundColor.value,
-					});
-				}}
-			/>
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': defaultArticleState.fontFamilyOption.value,
+					'--font-size': defaultArticleState.fontSizeOption.value,
+					'--font-color': defaultArticleState.fontColor.value,
+					'--container-width': defaultArticleState.contentWidth.value,
+					'--bg-color': defaultArticleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			<ArticleParamsForm />
 			<Article />
 		</main>
 	);
